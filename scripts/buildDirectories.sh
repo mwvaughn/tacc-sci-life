8220705bf8f43d233051d84c5c2140e5e2f5c314
johnfonner <johnfonner@gmail.com>
2015-09-02 01:14:16 -0500

    trying to update scripts

--- scripts/buildDirectories.sh
+++ scripts/buildDirectories.sh
@@ -2,20 +2,52 @@
 
 module load git
 
-scriptPath=$( cd "$( dirname $0 )" && pwd )
-if ! [[ "$scriptPath" =~ .*rpmbuild.* ]]; then 
-    echo "The script is running from the path $scriptPath"
-    echo "To setup directories, you need this script to reside somewhere inside an "rpmbuild" directory."
-    echo "We recommend cloning the tacc-sci-life repository into a directory called SPECS like this:"
-    echo "git clone https://github.com/mwvaughn/tacc-sci-life.git SPECS"
-    echo "Please see README.md for more information"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+usage="Usage: $0 <dir>"
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    echo $usage
+    exit 0
+fi
+
+if [ $# -gt 1 ]; then
+    echo "Error: Too many arguments."
+    echo $usage
     exit 1
 fi
 
-rpmbuildPath="${scriptPath%rpmbuild*}rpmbuild"
+if [ $# -eq 1 ]; then
+    baseDir=$1
+else
+    echo "Build directory tree in the current directory? (y/n)[n] "
+    read userInput
+    if [ "$userInput" == "y" ] || [ "$userInput" == "Y" ]
+        baseDir=$DIR
+    else
+        echo "Exiting..."
+        exit 0
+    fi
+fi
+
+if [ ! -d $baseDir ]; then
+    echo "Creating directory: $baseDir"
+fi
+
+
+# if ! [[ "$scriptPath" =~ .*rpmbuild.* ]]; then 
+#     echo "The script is running from the path $scriptPath"
+#     echo "To setup directories, you need this script to reside somewhere inside an "rpmbuild" directory."
+#     echo "We recommend cloning the tacc-sci-life repository into a directory called SPECS like this:"
+#     echo "git clone https://github.com/mwvaughn/tacc-sci-life.git SPECS"
+#     echo "Please see README.md for more information"
+#     exit 1
+# fi
+
+rpmbuildPath=$baseDir
 # echo $rpmbuildPath
 
-for systemName in ls4 stampede maverick; do
+for systemName in ls4 stampede maverick wrangler; do
     if [ ! -d $rpmbuildPath/$systemName ]; then
         echo "creating $systemName directory"
         mkdir -p $rpmbuildPath/$systemName
